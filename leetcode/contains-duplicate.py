95b824082d46392c8a86bce835158cd61d7427a3
aditi <chaudharyyaditi@gmail.com>
2026-05-20 15:18:04 -0400

    Update solution for contains-duplicate

--- leetcode/contains-duplicate.py
+++ leetcode/contains-duplicate.py
@@ -4,6 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+    # brute force
+    #    for i in range (len(nums)):
+    #        target = nums[i]
+    #        for j in range (i+1, len(nums)):
+    #            if nums[j] == target:
+    #                return True
+            
+    #    return False
+    # we can use sets for this since sets only stotre unique
         seen = set()
         for num in nums:
             if num in seen:
