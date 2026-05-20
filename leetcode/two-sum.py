95e2e5ed186deec10ca2a7b831359d6ec7391efa
aditi <chaudharyyaditi@gmail.com>
2026-05-20 13:17:26 -0400

    Update solution for two-sum

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,8 +1,15 @@
 class Solution(object):
+# brute force:
+#    def twoSum(self, nums, target):
+#        for i in range(len(nums)):
+#            for j in range(i + 1, len(nums)):
+#                if nums[i] + nums[j] == target:
+#                    return [i, j]
+# instead of searching through each pair, we could store numbers we've already seen and instantly check if needed partner exists
     def twoSum(self, nums, target):
-        seen = {} 
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+        seen = {}
+        for i in range(len(nums)):
+            needed = target - nums[i]
+            if needed in seen:
+                return [seen[needed], i]
+            seen[nums[i]] = i
